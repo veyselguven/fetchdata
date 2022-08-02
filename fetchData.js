416e9c4cd419c2189b4c09996cca15cea937aa2c
veysel basbaydar <vgbasbaydar@gmail.com>
2022-08-01 19:59:27 -0700

    example

--- fetchData.js
+++ fetchData.js
@@ -1,1 +1,26 @@
-console.log("naber");
+// fetch("https://reqres.in/api/users")
+//   .then((res) => {
+//     if (res.ok) {
+//       console.log("SUCCES");
+//     } else {
+//       console.log("Not Succesful");
+//     }
+//   })
+//   .then((data) => console.log("data", data))
+//   .catch((e) => console.log("e", e));
+
+///
+fetch("https://reqres.in/api/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    name: "User 1",
+  }),
+})
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => console.log("data", data))
+  .catch((e) => console.log("e", e));
