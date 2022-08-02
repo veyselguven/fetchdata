0d0446c4611e59becb12a0ade5a83b420107ec9b
veysel basbaydar <vgbasbaydar@gmail.com>
2022-08-02 14:05:52 -0700

    fetch2

---
+++ fetch2.js
@@ -0,0 +1,13 @@
+// let myInit = {
+//   method: "GET",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   mode: "cors",
+//   cahce: "default",
+// };
+// let myRequest = new Request("./datajson.json", myInit);
+
+// fetch(myRequest)
+//   .then((res) => res.json())
+//   .then((data) => console.log(data.student));
--- fetchData.js
+++ fetchData.js
@@ -10,17 +10,23 @@
 //   .catch((e) => console.log("e", e));
 
 ///
-fetch("https://reqres.in/api/users", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    name: "User 1",
-  }),
-})
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => console.log("data", data))
-  .catch((e) => console.log("e", e));
+// fetch("https://reqres.in/api/users", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     name: "User 1",
+//   }),
+// })
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data) => console.log("data", data))
+//   .catch((e) => console.log("e", e));
+
+fetch("https://reqres.in/api/users").then((res) => {
+  console.log(res);
+});
+// .then((data) => console.log("data", data))
+// .catch((e) => console.log("e", e));
